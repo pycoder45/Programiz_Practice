4dcb22494060fa88fef6616819ab8343bafd00fa
pycoder45 <pycoder45@gmail.com>
2021-07-08 14:50:31 +0530

    1

---
+++ Day_01.py
@@ -0,0 +1,28 @@
+
+# 1.Python Program to Print Hello world!
+print("Hello World")
+
+#2.Python Program to Add Two Numbers
+a = 2
+b = 8
+print(a + b)
+
+# 3.Python Program to Find the Square Root
+a = 64
+Square_root = a**1/2
+
+print(Square_root)
+
+# 4.Python Program to Calculate the Area of a Triangle
+base = 5
+height = 10 
+Area_Of_Tri = base*height*1/2
+
+print(Area_Of_Tri)
+
+# 5.Python Program to Solve Quadratic Equation
+
+a = 5
+b = 8
+c = 10
+
